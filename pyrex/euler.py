5590479f5082182650ab2014db5358b78c7959a9
WDerricotte <wallace.derricotte@emory.edu>
2019-01-02 08:21:27 -0500

    Fixed bug with mass-weighted cartesians for euler code

--- pyrex/euler.py
+++ pyrex/euler.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import numpy as np
 import psi4
 import os
@@ -33,32 +35,23 @@ class Params():
         # Read in input from JSON input file
         json_data=open(json_input).read()
         input_params = json.loads(json_data)
-        #print("DATA READ")
         if input_params['molecule']['geometry']:
             self.geometry = self.json2xyz(input_params)
         if input_params['model']['basis']:
             self.basis = input_params['model']['basis']
-            #self.level_of_theory = "%s/%s" %(self.method,self.basis)
         if input_params['model']['method']:
             self.method = input_params['model']['method']
-            #self.level_of_theory = "%s/%s" %(self.method,self.basis)
         if input_params['molecule']['symbols']:
             self.symbols = input_params['molecule']['symbols']
             self.natoms = len(input_params['molecule']['symbols'])    
-        if input_params['dvv']['cons_vel']:
-            self.cons_vel = input_params['dvv']['cons_vel']
-        if input_params['dvv']['err_tol']:
-            self.err_tol = input_params['dvv']['err_tol']
-        if input_params['dvv']['e_conv']:
-            self.e_conv = input_params['dvv']['e_conv']
-        if input_params['dvv']['direction']:
-            self.direction = input_params['dvv']['direction']
-        if input_params['dvv']['mode']:
-            self.mode = input_params['dvv']['mode']
-        if input_params['dvv']['mode_freq']:
-            self.mode_freq = input_params['dvv']['mode_freq']
-        if input_params['dvv']['normal_mode_file']:
-            self.normal_mode_file = input_params['dvv']['normal_mode_file']
+        if input_params['irc']['direction']:
+            self.direction = input_params['irc']['direction']
+        if input_params['irc']['step_size']:
+            self.step_size = input_params['irc']['step_size']
+        if input_params['irc']['mode']:
+            self.mode = input_params['irc']['mode']
+        if input_params['irc']['normal_mode_file']:
+            self.normal_mode_file = input_params['irc']['normal_mode_file']
             self.ts_vec = self.normal_mode_reader()
     def normal_mode_reader(self):
         natoms = self.natoms
@@ -67,21 +60,16 @@ class Params():
         ts_vec = []
         for line in normal_mode_file:
             if("vibration %d" %self.mode in line):
-                #print(line)
                 line = next(normal_mode_file)
-                #print(line)
                 for i in range(natoms):
                     trj = line.split()
-                    #print(trj)
                     trj = list(map(float, trj))
                     np_trj = np.array(trj)
-                    #print(np_trj)
                     if(direction=='backward'):
                         ts_vec.append(-1.0*np_trj)
                     else:
                         ts_vec.append(np_trj)
                     line = next(normal_mode_file)
-        #print(ts_vec)
         return ts_vec
 
 ########################
@@ -90,31 +78,42 @@ class Params():
 
 def grad_calc(params,current_geom, mol):
     mol.set_geometry(psi4.core.Matrix.from_array(current_geom))
-    #print(np.asarray(mol.geometry))
     grad_method = "%s/%s" %(params.method,params.basis)
     psi4.core.set_output_file("psi4_out.dat", False)
     E, wfn = psi4.energy(grad_method,return_wfn=True)
     grad = np.asarray(psi4.gradient(grad_method,ref_wfn=wfn))
-    grad_mw = mass_weight(params, grad)
+    grad_mw = mass_weight(params.natoms, grad)
     return grad_mw, E
 
-def mass_weight(params,grad):
-    #mol = psi4.geometry(params.geometry)
-    grad_mw = np.zeros((params.natoms, 3))
-    for i in range(params.natoms):
+def mass_weight(natoms,grad):
+    grad_mw = np.zeros((natoms, 3))
+    for i in range(natoms):
         for j in range(3):
-            grad_mw[i][j] = grad[i][j]*np.sqrt(mol.mass(i))
+            grad_mw[i][j] = grad[i][j]/np.sqrt(mol.mass(i))
     return grad_mw
 
-def euler_step(current_geom,grad):
+def mass_weight_geom(natoms,geom):
+    coord_mw = np.zeros((natoms, 3))
+    for i in range(natoms):
+        for j in range(3):
+            coord_mw[i][j] = geom[i][j]*np.sqrt(mol.mass(i))
+    return coord_mw
+
+def un_mass_weight_geom(natoms,geom_mw):
+    coord = np.zeros((natoms, 3))
+    for i in range(natoms):
+        for j in range(3):
+            coord[i][j] = geom_mw[i][j]/np.sqrt(mol.mass(i))
+    return coord
+
+def euler_step(natoms,current_geom,grad,step_size):
+    current_geom = mass_weight_geom(natoms, current_geom)
     grad_norm = np.asarray(np.linalg.norm(grad))
-    #print(grad_norm)
-    #print(grad)
-    #ratio = grad/grad_norm
-    current_geom -= 0.01*(grad/grad_norm)
+    current_geom -= step_size*(grad/grad_norm)
+    current_geom = un_mass_weight_geom(natoms, current_geom)
     return current_geom
 
-max_steps = 480
+max_steps = 1000
 params = Params()
 
 mol = psi4.geometry(params.geometry)
@@ -130,19 +129,17 @@ current_geom = np.asarray(mol.geometry())
 while (steps <= max_steps):
     mol.save_xyz_file('euler_step_'+str(steps)+'.xyz',False)
     if(steps==0):
-        grad = mass_weight(params, starting_vec)
+        grad = mass_weight(params.natoms, starting_vec)
+        #grad = starting_vec
     else:
         grad, E  = grad_calc(params, current_geom, mol)
 
-    current_geom = euler_step(current_geom, grad)
+    current_geom = euler_step(params.natoms, current_geom, grad,params.step_size)
     if(steps > 25):
         if(E>previous_E):
-            print(E)
-            print("New energy is greater!")
+            print("pyREX: New energy is greater! Likely near a minimum!")
             break
-    print(current_geom)
     steps = steps + 1
-    print(E)
     previous_E = E
 with open('irc_%s.xyz' %params.direction,'w') as outfile:
     for i in range(steps):
