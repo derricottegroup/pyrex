d45fcd1b7894fd7aa1392f8f891ca4ddaa85f597
root <root@localhost.localdomain>
2019-04-12 16:05:02 -0400

    Starting to add FSAPT capabilities

--- pyrex/sapt_class.py
+++ pyrex/sapt_class.py
@@ -1,3 +1,4 @@
+import os
 import psi4
 import numpy as np
 
@@ -29,9 +30,18 @@ class sapt(object):
             psi4.core.set_output_file("psi4_output/irc_%d_sapt.out" %count, False)
             geometry += "symmetry c1"
             psi4.geometry(geometry)
-            psi4.set_options({'reference': 'rhf', 'basis' : self.basis, 'exch_scale_alpha' : 'true'})
+            # Adds fsapt capabilities 
+            try:
+                os.mkdir('fsapt_output')
+            except FileExistsError:
+                pass
+            psi4.set_options({'reference': 'rhf', 'basis' : self.basis, 'exch_scale_alpha' : 'true', 'FISAPT_FSAPT_FILEPATH' :'fsapt_output/fsapt%d' %count})
             #print("pyREX:SAPT Calculation on IRC Point %d" %(count))
             e_int = psi4.energy(self.method)
+            #TODO: Actually have this create the SAPT files necessary for FSAPT partitioning. Make user options as well
+            #      for fragment definitions. Something in the sapt block. 
+            fsaptA_outfile = open('fsapt_output/fsapt%d/fA.dat' %count, 'w+')
+            fsaptB_outfile = open('fsapt_output/fsapt%d/fB.dat' %count, 'w+')
             e_elst = psi4.core.get_variable("SAPT ELST ENERGY")
             e_exch = psi4.core.get_variable("SAPT EXCH ENERGY")
             e_ind  = psi4.core.get_variable("SAPT IND ENERGY")
